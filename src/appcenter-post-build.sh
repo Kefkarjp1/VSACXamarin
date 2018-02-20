3646e8f71557bb79c5dfd2777efdd4307d8d9d4c
pqa <pqa@pqas-MacBook-Pro.local>
2018-02-20 11:36:03 -0400

    added post build script

---
+++ src/appcenter-post-build.sh
@@ -0,0 +1,5 @@
+﻿appcenter login --token $VSAC_Token
+
+appcenter test run uitest --app "PLATOTesting/XamarinApp-iOS" --devices 08dd42ea --app-path "$APPCENTER_SOURCE_DIRECTORY/src/ios/bin/iphone/$APPCENTER_BRANCH/VSACXamarin.ios.ipa"  --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/src/VSACXamarin.UITest.Ios/bin/release"
+
+appcenter logout
