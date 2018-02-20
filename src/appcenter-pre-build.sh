0312a0047c9c7c8ffa2f9f1e0e21e3250a5e36ee
pqa <pqa@pqas-MacBook-Pro.local>
2018-02-20 09:19:25 -0400

    add environment variables

--- src/appcenter-pre-build.sh
+++ src/appcenter-pre-build.sh
@@ -3,3 +3,5 @@ if [ "$APPCENTER_BRANCH" == "QA" ];
 then
 /usr/libexec/plistbuddy -c "Set :CfBundleDisplayName VSACXamarin.$APPCENTER_BRANCH" "iOS/Info.plist"
 fi
+
+sed -i -e "s/\[VSAC_APIKEY]/$VSAC_Key/g" Constants.cs
